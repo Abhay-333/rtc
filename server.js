7d8e2e6305330ff97609137266366c2b003727aa
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-05 14:07:51 +0530

    feat: server setup

---
+++ server.js
@@ -0,0 +1,5 @@
+import app from "./src/app.js";
+let port=process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log(`server is running on port  ${port}`)
+})
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express  from 'express'
+let app=express()
+export default app
